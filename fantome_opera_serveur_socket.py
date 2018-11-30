be6ee9f36ab4b2ac8231ab20e374b0f3ac69911b
Louis Ventre <louis.ventre@epitech.eu>
2018-11-30 15:03:37 +0100

    fix shebang

--- fantome_opera_serveur_socket.py
+++ fantome_opera_serveur_socket.py
@@ -1,4 +1,4 @@
-!#/usr/bin/env python
+#!/usr/bin/env python
 
 from random import shuffle,randrange
 from time import sleep
--- ghostAgent.py
+++ ghostAgent.py
@@ -1,4 +1,4 @@
-!#/usr/bin/env python
+#!/usr/bin/env python
 
 import runnerSocket
 from Parsing import PLAYER_TYPE
